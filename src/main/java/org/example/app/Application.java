cf2dfa5142225c63c20e7182235de5a23418a071
Rishi <rishi.mishra@credable.in>
2022-02-22 10:03:54 +0530

    Introduced AppConfig which will take care of bean lifecycle

--- src/main/java/org/example/app/Application.java
+++ src/main/java/org/example/app/Application.java
@@ -1,35 +1,30 @@
 package org.example.app;
 
+import org.example.AppConfig;
 import org.example.model.Customer;
 import org.example.repository.CustomerRepositoryDB;
 import org.example.repository.CustomerRepositoryStub;
 import org.example.service.CustomerService;
 import org.example.service.DefaultCustomerService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.util.List;
 
-// IoC container
 public class Application {
 
   public static void main(String[] args) {
 
-    // DESIGN PRINCIPLE: Always program to interface and not to an implementation
-
-    // wiring your application
-    CustomerService service = new DefaultCustomerService(newCustomerRepositoryStub());
+//    CustomerService service = new DefaultCustomerService(newCustomerRepositoryStub());
 
 //    List<Customer> customers = service.getAllCustomersWithStars();
+
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    CustomerService service = context.getBean("customerService", CustomerService.class);
     List<Customer> customers = service.getAllCustomers();
 
     customers.forEach(System.out::println);
   }
-
-  public static CustomerRepositoryDB newCustomerRepositoryDB() {
-    return new CustomerRepositoryDB();
-  }
-
-  public static CustomerRepositoryStub newCustomerRepositoryStub() {
-    return new CustomerRepositoryStub();
-  }
-
+  
 }
